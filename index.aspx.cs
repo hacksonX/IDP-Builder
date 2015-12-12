8b53d843ca13bb5631a37d97863685c3aa5410b2
hacksonx <mbhiza.shaun@gmail.com>
2015-12-13 01:48:07 +0200

    Shows basics of working with a MongoDB

---
+++ index.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+public partial class index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        makeTable();
+    }
+
+    public async void makeTable()
+    {
+        IMongoClient _client;
+        IMongoDatabase theDB;
+
+        _client = new MongoClient();
+        theDB = _client.GetDatabase("idpDB");
+        var myDOc = new BsonDocument
+        {
+            {"Population_growth", new BsonDocument
+                {
+                    { "Municipality", new BsonArray {"Bloudberg", "Aganang", "Polokwane", "Lepelle-Nkumpi", "Capricorn"}},
+                    { "2001 Population", new BsonArray {171.721, 146.872, 109.441, 508.277, 227.970, 1164281 } },
+                    { "2011 Population", new BsonArray {162629, 131164, 108321, 628999,230350, 1261463} },
+                    { "Population Growth Rate (2001-2011)", new BsonArray {-0.54, -1.13, -0.1, 2.13, 0.1, 0.8 } }
+
+
+                }
+             }
+        };
+        var collection = theDB.GetCollection<BsonDocument>("Population_Growth");
+        await collection.InsertOneAsync(myDOc);
+        someData.InnerHtml = "Works";
+    }
+}
---
+++ viewData.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+public partial class viewData : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        getData();
+    }
+
+    public async void getData()
+    {
+        IMongoClient _client;
+        IMongoDatabase theDB;
+
+        _client = new MongoClient();
+        theDB = _client.GetDatabase("idpDB");
+        var collection = theDB.GetCollection<BsonDocument>("Population_Growth");
+        var filter = new BsonDocument();
+        var count = 0;
+        someData.InnerHtml = "";
+        using (var cursor = await collection.FindAsync(filter))
+        {
+            while (await cursor.MoveNextAsync())
+            {
+                var batch = cursor.Current;
+                foreach (var document in batch)
+                {
+                    someData.InnerHtml += document.First().Name.ToString() + "<br/>";
+                    someData.InnerHtml += document.First().Value.ToString() + "<br/>";
+                    someData.InnerHtml += document.Last().Name.ToString() + "<br/>";
+                    someData.InnerHtml += document.Last().Value.ToString() + "<br/>";
+                    count++;
+                }
+            }
+        }
+    }
+}
